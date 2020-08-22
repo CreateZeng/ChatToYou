0152afe009c4daf1c9db49c8141ad26e81a4bd97
e <18807040491@163.com>
2020-08-23 02:03:43 +0800

    修改用户模块名字

---
+++ UserServer/src/main/java/com/zeng/pojo/ReturnResult.java
@@ -0,0 +1,32 @@
+package com.zeng.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @Author：Zeng-Jin
+ * @Motto：不积跬步、无以至千里。
+ * @Date：2020-08-19
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReturnResult implements Serializable{
+
+    private Integer state;
+
+    private String msg;
+
+    private Object data;
+
+    public static ReturnResult getSuccess(Object data){
+        return new ReturnResult(1,"操作成功",data);
+    }
+
+    public static ReturnResult getFail(Object data){
+        return new ReturnResult(0,"操作失败",data);
+    }
+}
