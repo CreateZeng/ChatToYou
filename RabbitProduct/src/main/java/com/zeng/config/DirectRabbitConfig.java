bdaab859e7c8d1192fd7a58b0d295e235e9715bd
CH-Zeng <18807040491@163.com>
2020-10-07 00:45:00 +0800

    add Dockerfile

---
+++ RabbitProduct/src/main/java/com/zeng/config/DirectRabbitConfig.java
@@ -0,0 +1,11 @@
+package com.zeng.config;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author：Zeng-Jin
+ * @Motto：不积跬步、无以至千里。
+ * @Date：2020-09-09
+ **/@Component
+public class DirectRabbitConfig {
+}
