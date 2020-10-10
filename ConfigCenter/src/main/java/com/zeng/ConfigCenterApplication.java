56394557333e8e62d649536522db77309a4994ce
CH-Zeng <18807040491@163.com>
2020-10-10 18:29:46 +0800

    配置中心注册到Eureka

--- ConfigCenter/src/main/java/com/zeng/ConfigCenterApplication.java
+++ ConfigCenter/src/main/java/com/zeng/ConfigCenterApplication.java
@@ -1,8 +1,14 @@
 package com.zeng;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import java.net.Inet4Address;
+import java.net.InterfaceAddress;
+import java.net.UnknownHostException;
 
 
 /**
@@ -13,9 +19,12 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  **/
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigCenterApplication {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws UnknownHostException {
+        System.out.println("本机为-----"+Inet4Address.getLocalHost().getHostAddress());
         SpringApplication.run(ConfigCenterApplication.class,args);
     }
 }
